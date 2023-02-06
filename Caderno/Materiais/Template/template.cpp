64515b93c55a98480402cea3ab62d7bc76658694
Pedro Oliveira <60803315+PedroVIOliv@users.noreply.github.com>
2023-02-06 20:59:37 -0300

    Criado

---
+++ Caderno/Materiais/Template/template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+//alias comp='g++ -std=c++17 -g -O2 -Wall -Wconversion -Wshadow -fsanitize=address,undefined -fno-sanitize-recover -ggdb -o out'
+
+#define sws std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); //Melhora o desempenho
+#define int long long //Melhor linha de codigo ja escrita
+#define endl "\n" //Evita flush
+#define loop(i,a,n) for(int i=a; i < n; i++)
+#define input(x) for (auto &it : x) cin >> it
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+#define mp make_pair
+#define TETO(a, b) ((a) + (b-1))/(b)
+#define dbg(msg, x) cout << msg << " = " << x << endl
+#define print(x,y) loop(i,0,y){cout << x[i] << " ";} cout << "\n";
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef priority_queue<int, vector<int>, greater<int>> pqi;
+
+const ll MOD = 1e9+7;
+const int MAX = 1e4+5;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+const double PI = acos(-1);
+
+
+int32_t main(){ sws; 
+    
+
+    return 0;
+}
