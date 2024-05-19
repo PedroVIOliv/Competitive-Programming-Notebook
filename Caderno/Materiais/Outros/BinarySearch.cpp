fc53518e48184942b37e23503c5ba35b14acd517
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2024-05-19 12:03:35 -0300

    corrections

--- Caderno/Materiais/Outros/BinarySearch.cpp
+++ Caderno/Materiais/Outros/BinarySearch.cpp
@@ -1,11 +1,10 @@
-int BinarySearch(<vector>int arr, int x){
+int BinarySearch(vector<int> &arr, int x){
     int k = 0;
     int n = arr.size();
 
     for (int b = n/2; b >= 1; b /= 2) {
         while (k+b < n && arr[k+b] <= x) k += b;
     }
-    if (arr[k] == x) {
+    if (arr[k] == x)
         return k;
-    }
 }
