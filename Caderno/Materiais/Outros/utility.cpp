f8a485b96a90b5a30882ed9fe0c00c7111693e92
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2024-06-03 14:32:12 -0300

    add more utility info

--- Caderno/Materiais/Outros/utility.cpp
+++ Caderno/Materiais/Outros/utility.cpp
@@ -11,3 +11,22 @@ nth_element(v.begin(), v.begin()+i, v.end());
 // ordenar o vetor de forma estável
 // O(nlog²n)
 stable_sort(v.begin(), v.begin());
+
+// Ler um número octal e printar
+scanf("%o", &num); printf("%o", num);
+
+// Ler um número hexadecimal e printar
+scanf("%x", &num); printf("%x", num);
+
+// converter uma string representando um número binário em número
+string num = "11"; bitset<32> bs(num);
+bs.to_ulong(); bs.to_ullong(); // unsigned long (long)
+
+// logs
+log(a) // base e
+log2(a) // base 2
+log10(a) // base 10
+log(a)/log(b) // log(a) na base b
+
+// numero de digitos de a na base b
+(int)floor(1 + log10((double)a)/log10((double) b));
